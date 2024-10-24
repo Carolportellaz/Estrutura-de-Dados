50110b264f9d0e6336d705b5542099806c40741f
Carolpz2005 <carolportellazanelato@gmail.com>
2024-10-24 08:48:27 -0300

    Update Atividade02.java

--- Atividade02.java
+++ Atividade02.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Atividade02 {
     static int total = 2;
-    public static int pot(int n){
+    public static int pot(int n) throws Exception{
         if(n > 1){
            total = total * 2; 
            return pot(n - 1);
@@ -17,13 +17,29 @@ public class Atividade02 {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Informe um número:");
         int num = teclado.nextInt();
-        int total = pot(num);
-        System.out.println("O total é: " + total);
+
+        if(num > 0){
+            try{
+                int total = pot(num);
+                System.out.println("O total é: " + total);
+            }
+    
+            catch(Exception e){
+                System.out.println("Ocorreu o seguinte erro: " + e.getMessage());
+            }
+        }
+
+        if(num == 0){
+            System.out.println("O total é: " + 1);
+        }
+
+        else{
+            System.out.println("O número informado precisa ser positivo");
+        }
 
         teclado.close();
     }
 }
-
 
 // 2 * 2 * 2 * 2
 
