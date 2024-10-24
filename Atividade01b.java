e94f9c88d7f1e12b0f19fa5603d8148365ca0ac7
Carolpz2005 <carolportellazanelato@gmail.com>
2024-10-24 08:27:45 -0300

    Update Atividade01b.java

--- Atividade01b.java
+++ Atividade01b.java
@@ -1,25 +1,41 @@
 import java.util.Scanner;
 
 public class Atividade01b {
+    int n1 = 0;
+    int n2 = 1;
+    int conta = 0;
+    int total = 1;
+
+    public int fib(int nUser) throws Exception{
+        if(nUser >= 0){
+            for(int i = 2; i <= nUser; i++){
+                conta = n1 + n2;
+                n1 = n2; 
+                n2 = conta;
+                total = total + conta;
+            }
+    
+            return total;
+        }
+        
+        return 0;
+    }
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int n1 = 0;
-        int n2 = 1;
-        int conta = 0;
-        int total = 1;
-
         System.out.println("Informe a quantidade de números");
         int nUser = teclado.nextInt();
+        Atividade01b atividade01b = new Atividade01b();
 
-        for(int i = 2; i <= nUser; i++){
-            conta = n1 + n2;
-            n1 = n2; 
-            n2 = conta;
-            total = total + conta;
+        try{
+            int total = atividade01b.fib(nUser);
+            System.out.println("O total é " + total);
+        }
+
+        catch(Exception e){
+            System.err.println("Ocorreu o seguinte erro " + e.getMessage());
         }
 
         teclado.close();
 
-        System.out.println("O total é " + total);
     }
 }
