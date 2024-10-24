0e91c7f5d600c143ea6e0162a7f084b19152e9cb
Carolpz2005 <carolportellazanelato@gmail.com>
2024-10-24 08:15:14 -0300

    Update Atividade01a.java

--- Atividade01a.java
+++ Atividade01a.java
@@ -1,31 +1,43 @@
 import java.util.Scanner;
 public class Atividade01a {
+    public int fat(int n) throws Exception{
+        int fat = 1; 
+            if((n > 0) && (n > 1)) {
+                for(int i = 0; i < n; i++){
+                    fat *= (n - i);
+                }
+            }
+    
+            else{
+                if(n == 0){
+                    fat = 0;
+                }
+    
+                else{
+                    if(n == 1){
+                        fat = 1;
+                    }
+                }
+            }
+
+        
+        return fat;
+    }
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Informe o número: ");
         int n = teclado.nextInt();
-        int fat = 1; 
-
-        if((n > 0) && (n > 1)) {
-            for(int i = 0; i < n; i++){
-                fat *= (n - i);
-            }
+        Atividade01a atividade01a  = new Atividade01a();
+        try{
+            int fat = atividade01a.fat(n);
+            System.out.println("O fatorial de " + n + " é: " + fat);
         }
 
-        else{
-            if(n == 0){
-                fat = 0;
-            }
-
-            else{
-                if(n == 1){
-                    fat = 1;
-                }
-            }
+        catch(Exception e){
+            System.out.println("Ocorreu o seguinte erro " + e);
         }
 
         teclado.close();
 
-        System.out.println("O fatorial de " + n + " é: " + fat);
     }
 }
