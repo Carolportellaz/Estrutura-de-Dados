c512ad6249534d2b45bbfc36c81f9466e22d73e9
Carolpz2005 <carolportellazanelato@gmail.com>
2024-11-01 11:55:03 -0300

    Update Atividade03C.java

--- Aula02/Atividade03C.java
+++ Aula02/Atividade03C.java
@@ -1,19 +1,16 @@
 import java.util.Scanner;
 public class Atividade03C {
-    int cot = 0;
-    public boolean func(int [] vetor, int n){
-
-        if(vetor.length > cot){
-            if(vetor[cot] == n){
-                return true;
-            }
-
-            else{
-                System.out.println(cot);
-                cot++;
-                return func(vetor, n);
-            }
+    int inicio = 0;
+    int fim = -1;
+    public boolean func(int [] vetor, int n) throws Exception{
+        fim = (vetor.length) / 2;
+        for(int i = 0; i < fim; i++){
+            
         }
+        
+        
+
+        
 
         return false;
         
@@ -23,15 +20,21 @@ public class Atividade03C {
         Atividade03C atividade03c = new Atividade03C();
         System.out.println("Informe um número: ");
         int n = teclado.nextInt();
-        int [] vetor = {100, 95, 20, 519, 200, 34};
-        boolean resultado = atividade03c.func(vetor, n); 
+        int [] vetor = {100, 95, 20, 12, 100, 519, 200, 34};
+        try{
+            boolean resultado = atividade03c.func(vetor, n); 
 
-        if(resultado == true){
-            System.out.println("O número está contido no vetor");
+            if(resultado == true){
+                System.out.println("O número está contido no vetor");
+            }
+    
+            if(resultado == false){
+                System.out.println("O número não está contido no vetor");
+            }
         }
 
-        if(resultado == false){
-            System.out.println("O número não está contido no vetor");
+        catch(Exception e){
+            System.out.println("Ocorreu o seguinte erro " + e.getMessage());
         }
 
         teclado.close();
