39e2e021d1d04ebd1f34d9b0021da8e3b76935e8
Carol <carolportellazanelato@gmail.com>
2024-11-04 23:39:35 -0300

    Update Atividade03C.java

--- Aula02/Atividade03C.java
+++ Aula02/Atividade03C.java
@@ -1,28 +1,51 @@
 import java.util.Scanner;
 public class Atividade03C {
-    int inicio = 0;
-    int fim = -1;
-    public boolean func(int [] vetor, int n) throws Exception{
-        fim = (vetor.length) / 2;
-        for(int i = 0; i < fim; i++){
-            
+    int var;
+
+    public boolean func(int [] vetor, int n, int fim, int inicio) throws Exception{
+        fim = fim / 2;
+            for(int i = 0; i <= fim; i++){
+                if(inicio < vetor.length){
+
+                    if(vetor[inicio] == n){
+                        var = n;
+                    }
+                }
+    
+                inicio++;
+    
+            }
+    
+
+        if(var == n){
+            return true;
         }
-        
-        
 
-        
+        else{
+            if(inicio < vetor.length){
+                return func(vetor, n, fim, inicio);
+                
+            }
 
-        return false;
-        
+            else{
+                return false;
+            }
+        }
+
+                
     }
     public static void main (String args[]){
         Scanner teclado = new Scanner(System.in);
         Atividade03C atividade03c = new Atividade03C();
+        int [] vetor = {45, 2, 7, 6, 45, 8, 54, 2};
         System.out.println("Informe um número: ");
         int n = teclado.nextInt();
-        int [] vetor = {100, 95, 20, 12, 100, 519, 200, 34};
+        int fim = vetor.length;
+        int inicio = 0;
+
+        
         try{
-            boolean resultado = atividade03c.func(vetor, n); 
+            boolean resultado = atividade03c.func(vetor, n, fim, inicio); 
 
             if(resultado == true){
                 System.out.println("O número está contido no vetor");
@@ -37,6 +60,8 @@ public class Atividade03C {
             System.out.println("Ocorreu o seguinte erro " + e.getMessage());
         }
 
+         
+        
         teclado.close();
 
     }
