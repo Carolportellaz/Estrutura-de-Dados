9fee66e9fc408e20e2f311528cc37bff493236ea
Carol <carolportellazanelato@gmail.com>
2024-12-04 10:18:23 -0300

    Update Lista.java

--- Aula05/Atividade01F/Lista.java
+++ Aula05/Atividade01F/Lista.java
@@ -1,61 +1,67 @@
 // A Lista terá os elementos e os métodos //
 public class Lista{
-    private No cabeca = new No();
-    private No cauda = cabeca;
-    int tamanho = 0;
+    public No cabeca = new No();
+    public No cauda = cabeca;
+    protected int tamanho = 0;
 
     public void adicionarI(Cliente c) throws Exception{
-        No elemento = new No(c, null, null);
-
+        No elemento =new No(c, null, null);
         if(tamanho == 0){
             System.out.println("Vazio");
-            cabeca.proximo = elemento;
+            this.cabeca.proximo = elemento;
             elemento.anterior = cabeca;
             elemento.proximo = null;
-            cauda = elemento;
+            this.cauda = elemento;
             tamanho++;
-
-
+            System.out.println("A cabeça é " + this.cabeca.proximo.elemento.getCodigo());
         }
 
         else{
-            No elemento_atual = cabeca.proximo;
-            System.out.println("A cabeça é " + cabeca.proximo);
+            System.out.println("O tamanho no else é:" + this.tamanho);
+            if(cabeca.proximo ==  null){
+                System.out.println("CABEÇA TA NULA!");
+            }
+            System.out.println("A cabeça é " + this.cabeca.proximo);
+
+            No elemento_atual = this.cabeca.proximo;
+            System.out.println("teste 1");
+
+            System.out.println("A cabeça é " + this.cabeca.proximo.elemento.getCodigo());
+            System.out.println("teste 2");
+
             // Preciso verificar se não é null se não dá erro //
-            // CRESCENTE // 
-            while( elemento_atual != null && c.getCodigo() > elemento_atual.elemento.getCodigo()){
-                // cabeça.próximo.próximo
+            // CRESCENTE //
+            
+            for(int i = 0; i < tamanho; i++){
+                //Inserie no inicio
+                if(c.getCodigo() < elemento_atual.elemento.getCodigo() && i == 0){
+                    System.out.println("Inserindo no início");
+                    elemento.proximo = elemento_atual;
+                    elemento.anterior = cabeca;
+                    elemento_atual.anterior = elemento;
+                    cabeca.proximo = elemento;
+                    tamanho++;
+                    break;
+                } 
+                //Insere no meio
+                if(elemento_atual.elemento.getCodigo() < c.getCodigo() && i < tamanho - 1 && i != tamanho - 1 && i != 0){
+                    insereMeio(elemento_atual, elemento);
+                    tamanho++;
+                    break;
+                }
+
+                if (elemento_atual.elemento.getCodigo() < c.getCodigo() && i == tamanho - 1){
+                    //Insere no fim
+                    System.out.println("Inserindo no final");
+                    elemento.anterior = cauda;
+                    elemento.proximo = null;
+                    cauda = elemento;
+                    cauda.anterior.proximo = elemento;
+                    tamanho++;
+                    break;
+                }
                 elemento_atual = elemento_atual.proximo;
             }
-
-            // Final da lista (dúvida) //
-            if(elemento_atual.proximo == null){
-                System.out.println("Inserindo no final");
-                cauda.proximo = elemento;
-                elemento.anterior = cauda;
-                cauda = elemento;
-            }
-            
-            // Inicío da lista //
-            if(elemento_atual == cabeca.proximo){
-                System.out.println("Inserindo no início");
-                elemento.proximo = elemento_atual;
-                elemento.anterior = cabeca;
-                cabeca.proximo = elemento;
-            }
-
-            // Meio da lista //
-            else{
-                System.out.println("Inserindo no meio");
-                elemento.proximo = elemento_atual;
-                elemento.anterior = elemento_atual.anterior;
-                elemento_atual.anterior = elemento;
-                elemento_atual.anterior.proximo = elemento;
-
-            }
-
-            tamanho++;
-
 
         }
 
@@ -65,9 +71,20 @@ public class Lista{
     public void imprimir(){
         System.out.println("Os clientes são: ");
 
+        No quebra_galho = cabeca.proximo;
+
         for(int i = 0; i < tamanho; i++){
-            System.out.println(cabeca.proximo.elemento.getCodigo() + " | " + cabeca.proximo.elemento.getNome() + " | " + cabeca.proximo.elemento.getTelefone());
-            cabeca.proximo = cabeca.proximo.proximo;
+            System.out.println(quebra_galho.elemento.getCodigo() + " | " + quebra_galho.elemento.getNome() + " | " + quebra_galho.elemento.getTelefone());
+            quebra_galho = quebra_galho.proximo;
         }
+    }
+
+    public void insereMeio(No elemento_atual, No novo_elemento){
+        System.out.println("Inserindo no meio");
+        No temp_no = elemento_atual.proximo;
+        elemento_atual.proximo = novo_elemento;
+        novo_elemento.anterior = elemento_atual;
+        novo_elemento.proximo = temp_no;
+        temp_no.anterior = novo_elemento;
     }
 }
