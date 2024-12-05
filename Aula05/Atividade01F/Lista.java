2eca5528966d1c99785d7d6bdbe7efe113656ef9
Carol <carolportellazanelato@gmail.com>
2024-12-04 21:56:40 -0300

    Update Lista.java

--- Aula05/Atividade01F/Lista.java
+++ Aula05/Atividade01F/Lista.java
@@ -1,90 +1,110 @@
-// A Lista terá os elementos e os métodos //
-public class Lista{
-    public No cabeca = new No();
-    public No cauda = cabeca;
-    protected int tamanho = 0;
+public class Lista {
+    No cabeca = new No();
+    No busca;
+    No cauda = cabeca;
+    int tamanho = 0;
+    No quebra_galho;
+    int posicao = 0;
 
-    public void adicionarI(Cliente c) throws Exception{
-        No elemento =new No(c, null, null);
-        if(tamanho == 0){
-            System.out.println("Vazio");
-            this.cabeca.proximo = elemento;
-            elemento.anterior = cabeca;
-            elemento.proximo = null;
-            this.cauda = elemento;
+    public void add(Cliente c) {
+        No novo_elemento = new No(c, null, null);
+        if (tamanho == 0) {
+            cabeca.proximo = novo_elemento;
+            novo_elemento.anterior = cabeca;
+            cauda = novo_elemento;
             tamanho++;
-            System.out.println("A cabeça é " + this.cabeca.proximo.elemento.getCodigo());
         }
 
-        else{
-            System.out.println("O tamanho no else é:" + this.tamanho);
-            if(cabeca.proximo ==  null){
-                System.out.println("CABEÇA TA NULA!");
-            }
-            System.out.println("A cabeça é " + this.cabeca.proximo);
+        else {
+            No quebra_galho = cauda;
 
-            No elemento_atual = this.cabeca.proximo;
-            System.out.println("teste 1");
-
-            System.out.println("A cabeça é " + this.cabeca.proximo.elemento.getCodigo());
-            System.out.println("teste 2");
-
-            // Preciso verificar se não é null se não dá erro //
-            // CRESCENTE //
-            
-            for(int i = 0; i < tamanho; i++){
-                //Inserie no inicio
-                if(c.getCodigo() < elemento_atual.elemento.getCodigo() && i == 0){
-                    System.out.println("Inserindo no início");
-                    elemento.proximo = elemento_atual;
-                    elemento.anterior = cabeca;
-                    elemento_atual.anterior = elemento;
-                    cabeca.proximo = elemento;
-                    tamanho++;
-                    break;
-                } 
-                //Insere no meio
-                if(elemento_atual.elemento.getCodigo() < c.getCodigo() && i < tamanho - 1 && i != tamanho - 1 && i != 0){
-                    insereMeio(elemento_atual, elemento);
-                    tamanho++;
-                    break;
-                }
-
-                if (elemento_atual.elemento.getCodigo() < c.getCodigo() && i == tamanho - 1){
-                    //Insere no fim
-                    System.out.println("Inserindo no final");
-                    elemento.anterior = cauda;
-                    elemento.proximo = null;
-                    cauda = elemento;
-                    cauda.anterior.proximo = elemento;
-                    tamanho++;
-                    break;
-                }
-                elemento_atual = elemento_atual.proximo;
+            while ((quebra_galho != cabeca)
+                    && (quebra_galho.elemento.getCodigo() > novo_elemento.elemento.getCodigo())) {
+                quebra_galho = quebra_galho.anterior;
+                posicao++;
             }
 
+            // Adicionar no inicio //
+            if (quebra_galho == cabeca) {
+                adicionarI(novo_elemento);
+            }
+
+            // Adicionar no final //
+            if(quebra_galho == cauda){
+                adicionarF(novo_elemento);
+            }
+
+            // Adicionar no meio //
+            else{
+                adicionarM(novo_elemento, quebra_galho);
+            }
         }
 
-   
     }
 
-    public void imprimir(){
-        System.out.println("Os clientes são: ");
+    public void adicionarI(No novo_elemento) {
+        // Lembrar de não usar métodos e variáveis externas como quebra-galho porque pode não funcionar //
+        novo_elemento.proximo = cabeca.proximo;
+
+        if(cabeca.proximo != null){
+            cabeca.proximo.anterior = novo_elemento;
+        }
+
+        cabeca.proximo = novo_elemento;
+        novo_elemento.anterior = cabeca;
+
+        // Não esquece de atualizar a cauda e o tamanho//
+        if(cauda == cabeca){
+            cauda = novo_elemento;
+        }
+
+        tamanho++;
+    }
+
+    public void adicionarF(No novo_elemento){
+        novo_elemento.anterior = cauda;
+        cauda.proximo = novo_elemento;
+        cauda = novo_elemento;
+
+        tamanho++;
+    }
+
+    public void adicionarM(No novo_elemento, No elemento_atual){
+        No quebra_galho = elemento_atual.proximo;
+
+        elemento_atual.proximo = novo_elemento;
+        novo_elemento.anterior = elemento_atual;
+        novo_elemento.proximo = quebra_galho;
+        
+        if(quebra_galho != null){
+            quebra_galho.anterior = novo_elemento;
+        }
+
+        tamanho++;
+    }
+
+    public void imprimir() {
+        System.out.println("Os clientes em ordem crescete são: ");
 
         No quebra_galho = cabeca.proximo;
 
-        for(int i = 0; i < tamanho; i++){
-            System.out.println(quebra_galho.elemento.getCodigo() + " | " + quebra_galho.elemento.getNome() + " | " + quebra_galho.elemento.getTelefone());
+        for (int i = 0; i < tamanho; i++) {
+            System.out.println(quebra_galho.elemento.getCodigo() + " | " + quebra_galho.elemento.getNome() + " | "
+                    + quebra_galho.elemento.getTelefone());
             quebra_galho = quebra_galho.proximo;
         }
     }
 
-    public void insereMeio(No elemento_atual, No novo_elemento){
-        System.out.println("Inserindo no meio");
-        No temp_no = elemento_atual.proximo;
-        elemento_atual.proximo = novo_elemento;
-        novo_elemento.anterior = elemento_atual;
-        novo_elemento.proximo = temp_no;
-        temp_no.anterior = novo_elemento;
+    public void imprimirD() {
+        System.out.println("Os clientes em ordem descrencete são: ");
+
+        No quebra_galho = cauda;
+
+        for (int i = 0; i < tamanho; i++) {
+            System.out.println(quebra_galho.elemento.getCodigo() + " | " + quebra_galho.elemento.getNome() + " | "
+                    + quebra_galho.elemento.getTelefone());
+            quebra_galho = quebra_galho.anterior;
+        }
     }
+
 }
