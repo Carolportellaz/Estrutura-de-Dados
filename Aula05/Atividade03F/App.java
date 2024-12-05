e80126d405ef83d0705a172b5c182f10b7cc44ce
Carol <carolportellazanelato@gmail.com>
2024-12-05 17:57:33 -0300

    Add files via upload

--- Aula05/Atividade03F/App.java
+++ Aula05/Atividade03F/App.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
-public class App{
+
+public class App {
     String nome;
     String pais;
     String atrativos;
     int nota;
     int escolha;
     Lista lista = new Lista();
-    int iteracao = 1;
+    int iteracao = 0;
+    int iteracao2 = 0;
+    boolean comeco = true;
+    boolean comeco2 = true;
+    No quebra_galho_prox;
+    No quebra_galho_ant;
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         App app = new App();
         app.mensagem();
     }
 
-    public int mensagem(){
+    public int mensagem() {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Você deseja: 1.Inserir | 2.Excluir | 3.Imprimir");
+        System.out.println("Você deseja: 1.Inserir | 2.Excluir | 3.Próximo | 4.Anterior");
         escolha = teclado.nextInt();
 
         switch (escolha) {
@@ -34,7 +40,7 @@ public class App{
                 System.out.println("Informe o nota");
                 nota = teclado.nextInt();
 
-                while(nota > 10 || nota < 0){
+                while (nota > 10 || nota < 0) {
                     System.out.println("Insira valores entre 0 e 10");
                     System.out.println("Informe o nota");
                     nota = teclado.nextInt();
@@ -47,23 +53,81 @@ public class App{
                 System.out.println("Está inserindo certo ");
 
                 try {
-                    lista.imprimirC();
+                    lista.imprimirD();
 
                 } catch (Exception e) {
                     System.out.println("Ocorreu o seguinte erro " + e.getMessage());
                 }
 
                 break;
-        
-            case 2: 
-            
-            case 3:
-            try {
-                lista.imprimirD(iteracao);
 
-            } catch (Exception e) {
-                System.out.println("Ocorreu o seguinte erro " + e.getMessage());
-            }
+            case 2:
+                System.out.println("Informe o país");
+                pais = teclado.next();
+
+                System.out.println("Informe o nome da cidade");
+                nome = teclado.next();
+
+                try{
+                    lista.remover2(pais, nome);
+                }
+
+                catch(Exception e){
+                    System.out.println("Ocorreu o seguinte " + e.getMessage());
+                }
+                break;
+
+            case 3:
+                try {
+                    quebra_galho_prox = lista.proximo(comeco, iteracao);
+
+                    if (quebra_galho_prox == null) {
+                        System.out.println("Não há próximo a esse valor");
+                    }
+
+                    else {
+                        System.out.println("O valor proximo é ");
+                        System.out.println(quebra_galho_prox.elemento.getNome() + " | "
+                                + quebra_galho_prox.elemento.getAtrativos() + " | "
+                                + quebra_galho_prox.elemento.getNota() + " | " + quebra_galho_prox.elemento.getPais());
+
+                        iteracao++;
+                        comeco = false;
+                        comeco2 = false;
+                    }
+
+                }
+
+                catch (Exception e) {
+                    System.out.println("Ocorreu o seguinte erro " + e.getMessage());
+                }
+
+                break;
+
+            case 4:
+                try {
+                    quebra_galho_ant = lista.anterior(comeco2, iteracao2);
+
+                    if(quebra_galho_ant == null){
+                        System.out.println("Não há valor anterior a esse");
+                    }
+
+                    else{
+                        System.out.println("O valor anterior é ");
+                        System.out.println(quebra_galho_ant.elemento.getNome() + " | "
+                                + quebra_galho_ant.elemento.getAtrativos() + " | "
+                                + quebra_galho_ant.elemento.getNota() + " | " + quebra_galho_ant.elemento.getPais());
+    
+                        iteracao2++;
+                        comeco2 = false;
+                    }
+                }
+
+                catch (Exception e) {
+                    System.out.println("Ocorreu o seguinte erro " + e.getMessage());
+                }
+
+                break;
 
             default:
                 break;
@@ -72,10 +136,9 @@ public class App{
         System.out.println("Você deseja realizar uma nova operação? 1.Sim | 2.Não");
         escolha = teclado.nextInt();
 
-        if(escolha == 1){
-            iteracao++;
+        if (escolha == 1) {
             return mensagem();
-        }   
+        }
 
         return 1;
     }
--- Aula05/Atividade03F/Lista.java
+++ Aula05/Atividade03F/Lista.java
@@ -3,81 +3,271 @@ public class Lista {
     public No cauda = cabeca;
     public int tamanho = 0;
     public int posicao = 0;
+    public int posicao2;
+    public boolean contem = false;
+    No novo_elemento;
 
-    public void add(Cidade c){
-        No novo_elemento = new No(c, null, null);
+    public void add(Cidade c) {
+        novo_elemento = new No(c, null, null);
 
         // Se a lista tá vazia //
-        if(tamanho == 0){
+        if (tamanho == 0) {
             cabeca.proximo = novo_elemento;
             novo_elemento.anterior = cabeca;
             cauda = novo_elemento;
             tamanho++;
         }
 
-        else{
-            novo_elemento.proximo = cabeca.proximo;
-            cabeca.proximo = novo_elemento;  
-            tamanho++;          
+        else {
+            No quebra_galho = cauda;
+
+            while ((quebra_galho != cabeca) && (quebra_galho.elemento.getNota() <= novo_elemento.elemento.getNota())) {
+                quebra_galho = quebra_galho.anterior;
+                posicao++;
+            }
+           
+            // Adicionar no inicio //
+            if (quebra_galho == cabeca) {
+                if (quebra_galho != null && quebra_galho.elemento != null){
+                    if(quebra_galho.elemento.getNota()>novo_elemento.elemento.getNota()){
+                        adicionarM(novo_elemento, quebra_galho);
+                    }
+                }
+                    else
+                adcionarI(novo_elemento);
+                
+            }
+
+            else {
+                if (quebra_galho == cauda) {
+                    adicionarF(novo_elemento);
+                }
+
+                else {
+                    adicionarM(novo_elemento, quebra_galho);
+                }
+            }
+
         }
 
     }
 
-    public void imprimirC(){
+    public void adcionarI(No novo_elemento) {
+        novo_elemento.proximo = cabeca.proximo;
+
+        if (cabeca.proximo != null) {
+            cabeca.proximo.anterior = novo_elemento;
+        }
+
+        cabeca.proximo = novo_elemento;
+        novo_elemento.anterior = cabeca;
+
+        if (cauda == cabeca) {
+            cauda = novo_elemento;
+        }
+
+        tamanho++;
+    }
+
+    public void adicionarF(No novo_elemento) {
+        novo_elemento.anterior = cauda;
+        cauda.proximo = novo_elemento;
+        cauda = novo_elemento;
+        tamanho++;
+    }
+
+    public void adicionarM(No novo_elemento, No elemento_atual) {
+        No quebra_galho = elemento_atual.proximo;
+        elemento_atual.proximo = novo_elemento;
+        novo_elemento.anterior = elemento_atual;
+        novo_elemento.proximo = quebra_galho;
+
+        if (quebra_galho != null) {
+            quebra_galho.anterior = novo_elemento;
+        }
+
+        tamanho++;
+    }
+
+    public void imprimirD() {
         No quebra_galho = cabeca.proximo;
-        for(int i = 0; i < tamanho; i++){
-            System.out.println("Todos os valores são: " + quebra_galho.elemento.getNome() + " | " + quebra_galho.elemento.getAtrativos() 
-            + "| " + quebra_galho.elemento.getNota() + " | " + quebra_galho.elemento.getPais());
+        for (int i = 0; i < tamanho; i++) {
+            System.out.println("Todos os valores são: " + quebra_galho.elemento.getNome() + " | "
+                    + quebra_galho.elemento.getAtrativos()
+                    + "| " + quebra_galho.elemento.getNota() + " | " + quebra_galho.elemento.getPais());
 
             quebra_galho = quebra_galho.proximo;
         }
     }
 
-    // Lembrar de colocar próxima e anterior //
-    public void imprimirD(int iteracao) throws Exception{
-        No quebra_galho = cabeca.proximo;
-        No maior = new No();
+    // Próximo //1
+    public No proximo(boolean comeco, int iteracao) throws Exception {
+        No quebra_galho2 = cabeca.proximo;
 
-        int inicio;
-        inicio = cabeca.proximo.elemento.getNota();
-        maior = quebra_galho;
+        System.out.println(iteracao);
+        quebra_galho2 = cabeca.proximo;
 
-        int maior2 = cabeca.proximo.elemento.getNota();
-        No no_maior = cabeca.proximo;
-        No no_incio = cabeca.proximo;
-
-
-        // Preciso encontrar o maior valor da lista //
-        if(iteracao == 1){
-            while(no_maior.proximo != null){
-                if(no_maior.elemento.getNota() > no_incio.elemento.getNota()){
-                    no_incio = no_maior.proximo;
-                    no_maior = no_incio;
-                }
-                no_maior = no_maior.proximo;
-            }
+        if (comeco == true) {
+            quebra_galho2 = cabeca.proximo;
+            return cabeca.proximo;
         }
 
-        /*if(iteracao == 1){
-            System.out.println("O maior é " + maior.elemento.getNota());
-            System.out.println("Os valores são: " + maior.elemento.getNome() + " | " + maior.elemento.getNota() + " | " 
-            + maior.elemento.getPais() + " | " + maior.elemento.getAtrativos());
+        for (int i = 0; i < iteracao; i++) {
+            quebra_galho2 = quebra_galho2.proximo;
+        }
+
+        return quebra_galho2;
+    }
+
+    // Anterior //1
+    public No anterior(boolean comeco2, int iteracao2) throws Exception {
+        No quebra_galho2 = cabeca.proximo;
+
+        if (comeco2 == true) {
+            return quebra_galho2;
+        }
+        for (int i = 0; i < iteracao2 - 1; i++) {
+            quebra_galho2 = quebra_galho2.proximo;
+        }
+
+        return quebra_galho2;
+    }
+
+
+    public void remover2(String pais,String cidade) throws Exception{
+        No quebra_galho=cabeca;
+        for(int i =0; i <tamanho;i++){
+            if(quebra_galho.proximo.elemento.getNome().equals(cidade)&& quebra_galho.proximo.elemento.getPais().equals(pais)){
+                if(i==0){
+                    if(quebra_galho.proximo==cauda){
+                        quebra_galho.proximo=null;
+                        cauda=quebra_galho;
+                        quebra_galho.anterior=cabeca;
+                    }else{
+                        try {
+                             removeInicio();
+                        } catch (Exception e) {
+                            // TODO: handle exception
+                        }
+
+                    }
+                }
+                else if(quebra_galho.proximo==cauda){
+                    quebra_galho.proximo=null;
+                    if(quebra_galho.anterior!=cabeca)
+                        quebra_galho.anterior=quebra_galho.anterior.anterior;
+                    cauda=quebra_galho;
+                }else  
+                {
+                    quebra_galho.proximo=quebra_galho.proximo.proximo;
+                    quebra_galho.proximo.anterior=quebra_galho.anterior;
+                    quebra_galho.proximo.proximo=quebra_galho;
+                    tamanho--;
+                break;
+                }
+            }else
+                 quebra_galho=quebra_galho.proximo;
+            System.out.println("dois");
+        }
+        
+    }
+    // Remover por nome e país //
+    public void remove(String pais, String cidade) throws Exception{
+        No quebra_galho = cabeca.proximo;
+
+        for(int i = 0; i < tamanho; i++){
+            if(quebra_galho.elemento.getPais().equals(pais) && (quebra_galho.elemento.getNome().equals(cidade))){
+                posicao = i;
+                contem = true;
+                break;
+            }
+            quebra_galho = quebra_galho.proximo;
+        }
+
+        if(contem == false){
+            throw new Exception();
+        }
+
+        if(contem == true){
+            if(posicao == 0){
+                System.out.println("Início");
+                removeInicio();
+            }
+
+            else if(posicao == tamanho-1){
+                System.out.println("Fim");
+                removeFim();
+            }
+
+            else{
+                System.out.println("Meio");
+                No el = cabeca.proximo;
+
+                for(int i = 1; i < posicao - 1; i++){
+                    el = el.proximo;
+                }
+
+                el.proximo = el.proximo.proximo;
+
+                if(el.proximo != null){
+                    el.proximo.anterior = el;
+                }
+
+                tamanho--;
+            }
         }
 
         else{
-            quebra_galho = cabeca.proximo;
-            while (quebra_galho.proximo != null) {
-                if(quebra_galho.elemento.getNota() >=  inicio){
-                    inicio = quebra_galho.elemento.getNota();
-                    maior = quebra_galho;
-                }
-    
-                System.out.println("Os valores são: " + maior.elemento.getNome() + " | " + maior.elemento.getNota() + " | " 
-                + maior.elemento.getPais() + " | " + maior.elemento.getAtrativos());
-                
-                quebra_galho = quebra_galho.proximo;
-            }
-        }*/
+            System.out.println("Não existe");
+        }
+
+
     }
+
+    public void removeInicio() throws Exception{
+        if (tamanho == 0) {
+            throw new Exception("Lista vazia");
+        }
+
+        cabeca.proximo = cabeca.proximo.proximo;
+
+        if (cabeca.proximo != null) {
+            cabeca.proximo.anterior = cabeca;
+        }
+
+        tamanho--;
+
+        if (tamanho == 0) {
+            cauda = null;
+        }
+    }
+
+    public void removeFim() throws Exception {
+        if (tamanho == 0) {
+            throw new Exception("Lista vazia");
+        }
+
+        if (tamanho == -1) {
+            removeInicio();
+        }
+
+        else {
+            No el = cabeca.proximo;
+            while (el.proximo != null) {
+                el = el.proximo;
+            }
+            el.anterior.proximo=cauda;
+            // Atualiza o último nó //
+            if (el.anterior != null) {
+                novo_elemento.anterior.proximo = null;
+
+
+            }
+            cauda = el.anterior.anterior;
+            tamanho--;
+        }
+
+    }
+    
 
 }
