ac966a08a618b37e65eafe35cb839d6f7a3942c2
Carol <carolportellazanelato@gmail.com>
2025-01-20 23:50:21 -0300

    Update App.java

--- Aula09/Atividade03/App.java
+++ Aula09/Atividade03/App.java
@@ -1,52 +1,60 @@
 import java.util.Scanner;
-public class App{
-    public static void main (String [] args){
+
+public class App {
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         int escolha = 0;
         Fila fila = new Fila();
-        Integer [] vet_r;
-        
-        while(escolha != 3){
-            System.out.println("Qual operação você deseja fazer: 1.Solicitar senha | 2.Chamar para atendimento | 3.Sair");
-            escolha = teclado.nextInt();
-            
-            switch (escolha) {
-                case 1:
-                    try {
-                        fila.enfilera();
-                        vet_r = fila.imprimir();
+        Integer[] vet_r;
 
-                        for(int i = fila.inicio; i >= fila.fim; i--){
-                            System.out.println(vet_r[i]);
+        try {
+            while (escolha != 3) {
+                System.out.println(
+                        "Qual operação você deseja fazer: 1.Solicitar senha | 2.Chamar para atendimento | 3.Sair");
+                escolha = teclado.nextInt();
+
+                switch (escolha) {
+                    case 1:
+                        try {
+                            fila.enfilera();
+                            vet_r = fila.imprimir();
+
+                            for (int i = fila.inicio; i >= fila.fim; i--) {
+                                System.out.println(vet_r[i]);
+                            }
                         }
-                    } 
-                    
-                    catch (Exception e) {
-                        System.out.println("Ocorreu o seguinte erro " + e.getMessage());
-                    }    
 
-                    break;
-                
-                case 2:
-                    try{
-                        System.out.println("Senha chamada " + fila.chamado());
-                        vet_r = fila.imprimir();
-
-                        for(int i = fila.inicio; i >= fila.fim; i--){
-                            System.out.println(vet_r[i]);
+                        catch (Exception e) {
+                            System.out.println("Ocorreu o seguinte erro " + e.getMessage());
                         }
-                    }
 
-                    catch(Exception e){
-                        System.out.println("Ocorreu o seguinte erro " + e.getMessage());
-                    }
-            
-                default:
-                    break;
+                        break;
+
+                    case 2:
+                        try {
+                            System.out.println("Senha chamada " + fila.chamado());
+                            vet_r = fila.imprimir();
+
+                            for (int i = fila.inicio; i >= fila.fim; i--) {
+                                System.out.println(vet_r[i]);
+                            }
+                        }
+
+                        catch (Exception e) {
+                            System.out.println("Ocorreu o seguinte erro " + e.getMessage());
+                        }
+
+                    default:
+                        break;
+                }
             }
         }
 
+        catch (Exception e) {
+            System.out.println("Ocorreu o seguinte erro " + e.getMessage());
+        }
+
         teclado.close();
-        
+
     }
 }
