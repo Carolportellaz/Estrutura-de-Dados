05f5a032b834872fadc60f8026a66af4c2c4fe6a
Carol <carolportellazanelato@gmail.com>
2025-01-31 16:06:39 -0300

    Update App.java

--- Aula11/Atividade01/App.java
+++ Aula11/Atividade01/App.java
@@ -1,40 +1,75 @@
 import java.util.Scanner;
-public class App{
+
+public class App {
     public static void main(String[] args) {
         int escolha = 1;
         String nome;
         Scanner teclado = new Scanner(System.in);
         Deque<String> deque = new Deque<>();
 
-        while(escolha != 0){
-            System.out.println("Informe o nome");
-            nome = teclado.nextLine();
-
-            System.out.println("Informe a operacao: 1.Adicionar início | 2. Adicionar fim | 3.Remover início | 4.Remover fim");
+        while (escolha != 0) {
+            System.out.println(
+                    "Informe a operacao: 1.Adicionar início | 2. Adicionar fim | 3.Remover início | 4.Remover fim ");
             escolha = teclado.nextInt();
 
-
-            switch (escolha) {
-                case 1:
-                    deque.addI(nome);
-                    break;
-            
-                default:
-                    break;
+            if (escolha == 0) {
+                break;
             }
-        }
+            try {
+                switch (escolha) {
+                    case 1:
+                        System.out.println("Informe o nome");
+                        nome = teclado.next();
+                        deque.addI(nome);
+                        break;
 
-        while(deque.isEmpty() == false){
-            try{
-                System.out.println(deque.removeI());
+                    case 2:
+                        System.out.println("Informe o nome");
+                        nome = teclado.next();
+                        deque.addF(nome);
+                        break;
+
+                    case 3:
+                        System.out.println(deque.removeI() + " foi removido");
+                        break;
+
+                    case 4:
+                        System.out.println(deque.removeF() + " foi removido");
+                        break;
+                    
+                    default:
+                        break;
+                }
             }
 
-            catch(Exception e){
+            catch (Exception e) {
                 System.out.println("Ocorreu o seguinte erro " + e.getMessage());
             }
+
         }
 
-        teclado.close();
+
+        try{
+            while (deque.tamanho > 0) {
+                try {
+                    System.out.println(deque.imprimir());
+                }
+
+                catch (Exception e) {
+                    System.out.println("Ocorreu o seguinte erro " + e.getMessage());
+                }
+            }
+
+        }
+
+        catch(Exception e){
+            System.out.println("Ocorreu o seguinte erro no imprimir" + e.getMessage());
+        }
         
+
+    
+
+        teclado.close();
+
     }
 }
