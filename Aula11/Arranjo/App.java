54bdcf5b16a0b4eca88183d1a0f4e0c85f45317a
Carol <carolportellazanelato@gmail.com>
2025-02-06 00:13:03 -0300

    Update App.java

--- Aula11/Arranjo/App.java
+++ Aula11/Arranjo/App.java
@@ -10,8 +10,9 @@ public class App{
         int escolha = 1;
         Deque deque = new Deque();
         NoDeque <Pessoa> [] vet = new NoDeque[10];
-
-
+        int inicio;
+        int tamanho;
+        int cot = 0;
 
         while(escolha != 0){
             System.out.println("Informe a operação: 1.Adicionar no início | 2.Adicionar no fim | 3.Remover do início | 4.Remover do fim  | 5.Impirmir");
@@ -34,14 +35,61 @@ public class App{
                 catch(Exception e){
                     System.out.println("Ocorreu o seguinte erro " + e.getMessage());
                 }
-                    
+                
+                case 2:
+                    System.out.println("Informe o nome: ");
+                    nome = teclado.next();
+        
+                    System.out.println("Informe a idade: ");
+                    idade = teclado.nextInt();
+                    deque.addF(nome, idade);
+                    break;
 
+                case 3:
+                    try{
+                        System.out.println("O valor " + deque.removerI());
+                    }
+
+                    catch(Exception e){
+                        System.out.println("Ocorreu o seguinte erro " + e.getMessage());
+                    }
+                    break;
                 case 5:
                     try{
                         vet = deque.imprimir();
+                        inicio = deque.inicio;
+                        tamanho = deque.tamanho;
+                        cot = 0;
 
-                        for(int i = deque.inicio; i >= deque.fim; i--){
-                            System.out.println(vet[i].getInfo().getNome() + " | " + vet[i].getInfo().getIdade());
+
+                        if(tamanho == 0){
+                            System.out.println("Lista vazia");
+                        }
+
+                        else{
+                            while(tamanho > 0){
+                                if(inicio != vet.length){
+                                    System.out.println(vet[inicio].getInfo().getNome() + " | " + vet[inicio].getInfo().getIdade());
+                                    inicio--;
+                                    tamanho--;
+                                }
+
+                                else{
+                                    if(cot == 0){
+                                        inicio = 0;
+                                        System.out.println(vet[inicio].getInfo().getNome() + " | " + vet[inicio].getInfo().getIdade());
+                                        cot++;
+                                        inicio--;
+                                        tamanho--;
+                                    }
+    
+                                    else{
+                                        System.out.println(vet[inicio].getInfo().getNome() + " | " + vet[inicio].getInfo().getIdade());
+                                        inicio--;
+                                        tamanho--;
+                                    }
+                                }
+                            }
                         }
                     }
 
@@ -49,6 +97,8 @@ public class App{
                         System.out.println("Ocorreu o seguinte erro " + e.getMessage());
                     }
                     
+                    break;
+
                 default:
                     break;
             }
