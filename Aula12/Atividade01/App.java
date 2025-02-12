b42a587cd1e4ab2aa3465602cf8f2f2daa1b3eec
Carol <carolportellazanelato@gmail.com>
2025-02-12 16:58:03 -0300

    Update App.java

--- Aula12/Atividade01/App.java
+++ Aula12/Atividade01/App.java
@@ -12,14 +12,25 @@ public class App {
         ht.putAberto(new Livro("978-0-7334-2609-4","A volta","Lobão",1200));
         ht.putAberto(new Livro("378-0-7334-2609-4","A ida","Lobão",1200));
         ht.putAberto(new Livro("978-0-7234-2609-4","Não volto","Lobão",1200));
-        ht.putAberto(new Livro("958-0-7234-2609-4","Recomeço","Lobão",1200));
-        
+        ht.putAberto(new Livro("178-0-7234-2609-4","Sentirei saudades","Lobão",1200));
+        ht.putAberto(new Livro("278-0-7234-2609-4","Voltei","Lobão",1200));
+        ht.putAberto(new Livro("478-0-7234-2609-4","Democracia","Lobão",1200));
+        ht.putAberto(new Livro("578-0-7234-2609-4","O grito","Lobão",1200));
+        ht.putAberto(new Livro("678-0-7234-2609-4","O silêncio","Lobão",1200));
+        ht.putAberto(new Livro("878-0-7234-2609-4","O crime","Lobão",1200));
+        ht.putAberto(new Livro("929-0-7234-2609-4","A vingança","Lobão",1200));
+        ht.putAberto(new Livro("998-0-7234-2609-4","A vingança II","Lobão",1200));
+
 
         // Removendo
         try{
             System.out.println("Removendo");
-            removido = ht.remove("958-0-7234-2609-4");
+            removido = ht.remove("978-0-7234-2609-4");
             System.out.println("O livro " + removido.getTitulo() + " foi removido");
+
+            removido = ht.remove("678-0-7234-2609-4");
+            System.out.println("O livro " + removido.getTitulo() + " foi removido");
+
         }
         
         catch(Exception e){
@@ -30,8 +41,10 @@ public class App {
         try{
             vet_livros = ht.imprimir();
 
-            for(int i = 0; i < ht.tamanho; i++){
-                System.out.println(vet_livros[i].getISBN() + " | " + vet_livros[i].getTitulo() + " | " + vet_livros[i].getAutor() + " | " + vet_livros[i].getPreco());
+            for(int i = 0; i < vet_livros.length; i++){
+                if(vet_livros[i] != null){
+                    System.out.println(vet_livros[i].getISBN() + " | " + vet_livros[i].getTitulo() + " | " + vet_livros[i].getAutor() + " | " + vet_livros[i].getPreco());
+                }
             }
         }
 
@@ -58,6 +71,8 @@ public class App {
         catch(Exception e){
             System.out.println("Ocorreu o seguinte erro " + e.getMessage());
         }
+
+        teclado.close();
 
     }
 }
