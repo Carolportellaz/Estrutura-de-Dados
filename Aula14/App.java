88c2f3a3322ac2da4439f3a63e4e99520ac0dd14
Carol <carolportellazanelato@gmail.com>
2025-03-07 00:03:56 -0300

    Update App.java

--- Aula14/App.java
+++ Aula14/App.java
@@ -4,23 +4,23 @@ public class App {
         lst.addInicio(new Pessoa("Elizabeth", 15));
         lst.addInicio(new Pessoa("Roberto Carlos", 78));
         lst.addInicio(new Pessoa("Joana", 16));
-        lst.addFim(new Pessoa("Cerberus", 12));
+        lst.addInicio(new Pessoa("Cerberus", 12));
         lst.addInicio(new Pessoa("Paulo Henrique",39));
-        lst.addFim(new Pessoa("Pandora", 12));
-        lst.add(3, new Pessoa("Guilhermina", 53));
-        lst.add(1, new Pessoa("Jovenilda", 53));
         lst.print();
         
         System.out.println();
         System.out.println();
-        try {
-            lst.removeFim();
-            lst.removeInicio();
-            lst.remove(2);
-            lst.remove(1);
-        } catch(Exception e){
-            System.out.println(e.getMessage());
+        
+
+        try{
+            lst.bubbleSort();
+            System.out.println("Valor com BobbleSort");
+            lst.print();
         }
-        lst.print();
+            
+
+        catch(Exception e){
+            System.out.println("Ocorreu o seguinte erro " + e.getMessage());
+        }
     }
 }
